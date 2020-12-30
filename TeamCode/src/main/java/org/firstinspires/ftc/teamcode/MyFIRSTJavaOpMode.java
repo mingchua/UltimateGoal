a2ad59bdf6e24cefdb8f33efa36a170b5d0031de
Mingchuan Liu <haohaomingsuper@gmail.com>
2020-12-29 22:35:03 -0800

    Program for intake/transfer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpMode.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Gyroscope;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
@@ -25,7 +26,7 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
     private DcMotor backLeft;
     private DcMotor backRight;
     private DcMotor intake;
-    private DcMotor transfer;
+    private DcMotorEx transfer;
 
     @Override
     public void runOpMode() {
@@ -35,7 +36,7 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
         frontRight = hardwareMap.get(DcMotor.class, "FrontRight");
         backLeft = hardwareMap.get(DcMotor.class, "BackLeft");
         backRight = hardwareMap.get(DcMotor.class, "BackRight");
-        transfer = hardwareMap.get(DcMotor.class, "transfer");
+        transfer = hardwareMap.get(DcMotorEx.class, "transfer");
         //resets encoders to zero
         frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -65,7 +66,7 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
         double maxAbsPower;
         double maxPower = 0.4;
         double righttrigger;
-        double lefttrigger;
+        boolean rightbumper;
         //while running
         while (opModeIsActive()) {
             // forward and backwards
@@ -74,7 +75,7 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
             y = -this.gamepad1.left_stick_y;
             rotation = -this.gamepad1.right_stick_x;
             righttrigger = this.gamepad1.right_trigger;
-            lefttrigger = this.gamepad1.left_trigger;
+            rightbumper = this.gamepad1.right_bumper;
 
             frontLeftPower = rotation - y - x;
             frontRightPower = rotation + y + x;
@@ -104,17 +105,18 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
 
             //runs intake and transfer when right trigger is pressed beyond 0.2, puny human
             if (righttrigger > 0.2) {
-                transfer.setPower(0.85);
+                transfer.setPower(-1);
             } else {
-                if (lefttrigger > 0.2) {
-                    transfer.setPower(-0.5);
+                if (rightbumper == true) {
+                    transfer.setVelocity(1200);
                 } else {
-                    transfer.setPower(0);
+                    transfer.setVelocity(0);
                 }
             }
 
             //logs for puny humans
             //sends power and position (degrees the wheels have spun) to driver station.
+            telemetry.addData("transfer power",transfer.getVelocity());
             telemetry.addData("frontLeftPower", (frontLeftPower));
             telemetry.addData("frontRightPower", (frontRightPower));
             telemetry.addData("backLeftPower", (backLeftPower));
