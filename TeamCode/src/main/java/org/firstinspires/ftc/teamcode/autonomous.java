50ab266798014a2de84d018d3d2de9742621ba94
Schwimmer <logan.schwimmer@gmail.com>
2021-02-15 21:03:14 -0800

    made changes to autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous.java
@@ -3,11 +3,12 @@ package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
-
+@Autonomous
 public class autonomous extends LinearOpMode {
     Servo elbow;
     Servo claw;
@@ -24,18 +25,22 @@ public class autonomous extends LinearOpMode {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
         // We want to start the bot at x: 10, y: -8, heading: 90 degrees
-        Pose2d startPose = new Pose2d(24, -60, Math.toRadians(0));
+        Pose2d startPose = new Pose2d(-63, -24, Math.toRadians(0));
 
         drive.setPoseEstimate(startPose);
 
         Trajectory traj1 = drive.trajectoryBuilder(startPose)
-                .splineTo(new Vector2d(16, -12), Math.toRadians(0))
-                .splineTo(new Vector2d(36, 36), Math.toRadians(0))
+                .splineTo(new Vector2d(-24, -12), Math.toRadians(0))
+                .splineTo(new Vector2d(36, -36), Math.toRadians(0))
                 .build();
 
         Trajectory traj2 = drive.trajectoryBuilder(traj1.end())
                 .splineTo(new Vector2d(36, 12), Math.toRadians(0))
                 .build();
+
+        waitForStart();
+
+        if(isStopRequested()) return;
 
         drive.followTrajectory(traj1);
         elbow.setPosition(ELBOW_DOWN_POS);
@@ -43,6 +48,6 @@ public class autonomous extends LinearOpMode {
         claw.setPosition(CLAW_OPEN_POS);
         sleep(100);
         elbow.setPosition(ELBOW_UP_POS);
-        drive.followTrajectory(traj2);
+        //drive.followTrajectory(traj2);
     }
 }
